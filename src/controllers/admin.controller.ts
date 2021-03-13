d5deba2699c6ecb4d5ec8c09594dd92f0c31bc95
Misbah Khalil <misbah.khalil.az@gmail.com>
2021-03-14 00:45:37 +0500

    minor fixes

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -1,43 +1,60 @@
-import * as express from 'express';
-import fs from 'fs'
-import xlsToJson from 'convert-excel-to-json';
-import User from '../interfaces/createUser';
-import {PrismaClient} from '@prisma/client'
+import * as express from "express";
+import fs from "fs";
+import xlsToJson from "convert-excel-to-json";
+import User from "../interfaces/createUser";
+import { PrismaClient } from "@prisma/client";
 
+export default class AdminController {
+  private prisma: PrismaClient = new PrismaClient();
 
-export default class AdminController{
+  public createStdAccounts = async (
+    req: express.Request,
+    res: express.Response
+  ) => {
+    try {
+      if (!req.file) {
+        throw {
+          error: "file not attached",
+        };
+      }
+      const students: Array<User> = await xlsToJson({
+        // convert uploaded xls to Json
+        sourceFile: req.file.path,
+        header: {
+          rows: 1,
+        },
+        columnToKey: {
+          A: "user_id",
+          B: "name",
+          C: "phone_no",
+          D: "email",
+        },
+      }).Sheet1;
 
-    private prisma: PrismaClient = new PrismaClient();
+      fs.unlink(req.file.path, () => console.log(req.file.path, "deleted")); // delete xls file after storing data to db
 
-    public createStdAccounts = async (req: express.Request, res: express.Response) => {
-        try {
-            const students: Array<User> = await xlsToJson({  // convert uploaded xls to Json
-                sourceFile: req.file.path,
-                header: {
-                    rows: 1
-                },
-                columnToKey: {
-                    A: 'user_id',
-                    B: 'name',
-                    C: 'phone_no',
-                    D: 'email'
-                }
-            }).Sheet1;
-
-            fs.unlink(req.file.path, () => console.log(req.file.path, "deleted")); // delete xls file after storing data to db
-        
-            if (students) {
-                this.prisma.users.createMany({ data: students.map(std => ({...std, phone_no: std.phone_no.toString()})), skipDuplicates: true })
-                    .then(status => res.status(200).send({message: status.count + " Records added."}))
-                    .catch(err => { throw { message: err } })
-            } else throw {
-                message: 'unable to read file'
-            }
-
-        } catch (error) {
-            console.log(error);
-            res.send(error)
-        }
+      if (students) {
+        this.prisma.users
+          .createMany({
+            data: students.map((std) => ({
+              ...std,
+              phone_no: std.phone_no.toString(),
+            })),
+            skipDuplicates: true,
+          })
+          .then((status) =>
+            res.status(200).send({ message: status.count + " Records added." })
+          )
+          .catch((err) => {
+            throw { error: err };
+          });
+      } else
+        throw {
+          error: "unable to read file",
+        };
+    } catch (error) {
+      console.log(error);
+      res.status(500).send(error);
     }
-         
+  };
 }
