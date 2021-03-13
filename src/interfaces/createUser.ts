541eeb2c530f6649994a93a365c52891cec8a718
Misbah Khalil <misbah.khalil.az@gmail.com>
2021-03-14 01:50:16 +0500

    create teachers account api

--- src/interfaces/createUser.ts
+++ src/interfaces/createUser.ts
@@ -1,6 +1,10 @@
-export default interface User{
-    user_id: string,       
-    name: string,  
-    phone_no: string,      
-    email: string,
+export interface User {
+  user_id: string;
+  name: string;
+  phone_no: string;
+  email: string;
+}
+
+export interface Teacher extends User {
+  role: string;
 }
